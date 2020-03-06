4e3b3cf63b2ced14c711853339fb1f3206983c1a
Gomango999 <kv.zhu999@gmail.com>
2020-03-06 22:39:46 +1100

    Added README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# MNIST Neural Network
+An implementation of a feed forward neural network to on the MNIST data set, with an error of 2.33% (97.66% accuracy). It consists of a single hidden layer of 800 neurons.
+
+### Usage
+To train and test a neural network from scratch, run
+```
+python3 mnist.py
+```
+
+To draw your own images and have the neural network classify them, run
+```
+python3 mnist_draw.py
+```
+
--- mnist.py
+++ mnist.py
@@ -3,19 +3,25 @@ from neural_network import *
 import util
 import random
 
-# LAYER_SIZES = [28*28,800,10]
-# BATCH_SIZE = 100
-# neural_network = NeuralNetwork(LAYER_SIZES, BATCH_SIZE, LeakyReLU(0.0), SoftmaxCrossEntropy())
+# Initialise a new neural network
+LAYER_SIZES = [28*28,800,10]
+BATCH_SIZE = 100
+neural_network = NeuralNetwork(LAYER_SIZES, BATCH_SIZE, LeakyReLU(0.0), SoftmaxCrossEntropy())
 
-neural_network = util.load("mnist.nn")
+# Load a neural network from memory
+#  neural_network = util.load("mnist.nn")
 
+# Train the neural network
 training_images = util.read_MNIST_image_file_ex("./data/train-images-idx3-ubyte")
 training_labels = util.read_MNIST_label_file("./data/train-labels-idx1-ubyte")
 training_data = list(zip(training_images, training_labels))
 
 neural_network.train(training_data)
+
+# Save a neural network to memory
 util.save(neural_network, "mnist.nn")
 
+# Test the neural network
 test_images = util.read_MNIST_image_file_ex("./data/t10k-images-idx3-ubyte")
 test_labels = util.read_MNIST_label_file("./data/t10k-labels-idx1-ubyte")
 test_data = list(zip(test_images, test_labels))
